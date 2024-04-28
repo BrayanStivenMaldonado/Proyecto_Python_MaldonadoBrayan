7b16a0d7a5e0d7ca6dcf8c7f9cf0a5e8cc40433e
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-04-28 15:27:40 -0500

    First

---
+++ Proyecto_Python_MaldonadoBrayan_LizarazoMaria.py
@@ -0,0 +1,23 @@
+#Proyecto para el filtro de Python
+
+import json
+
+def abrirArchivo(): #Función que va a servir para abrir el archivo
+    miJSON=[]
+    with open('mainIndex.json','r',encoding='utf-8') as openfile:
+        miJSON = json.load(openfile)
+    return miJSON
+
+def guardarArchivo(miData): #Función que va a servir para guardar los datos que se realicen al archivo
+    with open("mainIndex.json","w",encoding='utf-8') as outfile:
+        json.dump(miData,outfile)
+
+#INICIO DEL PROGRAMA
+print("BIENVENIDO AL PROGRAMA PARA EL FILTRO DE PYTHON DE BRAYAN Y MARIA")
+
+#Entrada en la que el usuario va a ingresar el rol que tiene para así saber qué opciones mostrarle
+RolUsuario = str(input("¿Cuál es su rol dentro de CampusLands?\n 1. Camper\n 2. Trainer\n 3. Coordinador\n"))
+print("Hola", RolUsuario)
+#El ROL de "Coordinador" Debe tener la opción de registrar la nota de los campers para cambiar el estado de "Aprobado" (Prom_PT_PP>=60 = Aprobado)
+
+#HORARIO 6am-10am, 10am-2pm, 2pm-6pm, 6pm-10pm
